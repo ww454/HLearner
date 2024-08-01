074e40c6548f97d615d646692ff90ad9abe47979
ww454 <weiwchen8-c@my.cityu.edu.hk>
2024-08-01 19:17:38 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+	$('#cssmenu #menu-button').on('click', function(){
+		var menu = $(this).next('ul');
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}
+	});
+});
+} )( jQuery );
+
+function openLoginForm() {
+    document.getElementById("loginForm").style.display = "block";
+}
+
+function closeLoginForm() {
+    document.getElementById("loginForm").style.display = "none";
+}
+
